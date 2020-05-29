63880698a47b666246f059f21993fb959ad502d8
saadamindar <saadamindar@live.com>
2020-05-29 22:20:19 +0300

    Updates

---
+++ precache-manifest.9d5063607cc5a9178dfcfb337b7019ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24cf938fc317d57ad93dd155d5cb1139",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d379f60e5fb1d4994c4e",
+    "url": "/monsters-rolodex/static/css/main.ee82f176.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d379f60e5fb1d4994c4e",
+    "url": "/monsters-rolodex/static/js/main.c5c62449.chunk.js"
+  },
+  {
+    "revision": "6062e4a584c29c6e0372",
+    "url": "/monsters-rolodex/static/js/runtime-main.e3d252a9.js"
+  }
+]);
